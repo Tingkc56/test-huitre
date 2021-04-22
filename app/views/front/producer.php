f34a43119aaf08c326d27ac8b69d705f4a4a64a5
Ting KC <ting.kc.56@gmail.com>
2021-04-22 11:55:24 +0200

    clean

--- app/views/front/producer.php
+++ app/views/front/producer.php
@@ -2,19 +2,20 @@
 <!-- fonction php pour injecter le header -->
 <section class="producer text-center">
 
-        <div class="article-page">
-    <div class="article-img flex-1-item-perline-s flex-2-item-perline-m">
-        <img class="img-fluid img-br-10px" src="<?= htmlspecialchars($producer['img']) ?>" alt="<?= htmlspecialchars($producer['alt']) ?>">
-    </div>
+    <div class="article-page">
+        <div class="article-img flex-1-item-perline-s flex-2-item-perline-m">
+            <img class="img-fluid img-br-10px" src="<?= htmlspecialchars($producer['img']) ?>"
+                alt="<?= htmlspecialchars($producer['alt']) ?>">
+        </div>
 
-    <div class="article-body flex-1-item-perline-s flex-2-item-perline-m">
-        <h1 class="text-center"><?= htmlspecialchars($producer['p_name']) ?></h1>
+        <div class="article-body flex-1-item-perline-s flex-2-item-perline-m">
+            <h1 class="text-center"><?= htmlspecialchars($producer['p_name']) ?></h1>
 
-        <h3>TEL: <?=htmlspecialchars($producer['tel']) ?></h3>
-        <h3>Adresse : <?=htmlspecialchars($producer['adresse']) ?></h3>
+            <h3>TEL: <?=htmlspecialchars($producer['tel']) ?></h3>
+            <h3>Adresse : <?=htmlspecialchars($producer['adresse']) ?></h3>
 
-        <p class="article-text"><?= htmlspecialchars($producer['p_content']) ?></p>
-
+            <p class="article-text"><?= htmlspecialchars($producer['p_content']) ?></p>
+        </div>
     </div>
 
 </section>
